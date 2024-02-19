16c84da4f9bcf28190c263497a145482fa03a0e6
zeekhoks <106755622+zeekhoks@users.noreply.github.com>
2024-02-19 17:08:45 -0500

    shell script fixes

--- app.sh
+++ app.sh
@@ -8,7 +8,8 @@ sudo apt-get install openjdk-19-jdk -y
 echo "Java installed successfully"
 echo "$(java --version) is the version of java"
 
+sudo mkdir /home/ubuntu/webapp
 sudo mv /tmp/unichef.jar /home/ubuntu/webapp
 sudo mv /tmp/unichef.service /etc/systemd/system/unichef.service
-sudo systemctl enable project.service
-sudo systemctl start project.service
+sudo systemctl enable unichef.service
+sudo systemctl start unichef.service
