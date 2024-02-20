62e3d99d3ec3459aa03c7a8ad32cf75220d60a1b
zeekhoks <106755622+zeekhoks@users.noreply.github.com>
2024-02-19 20:14:23 -0500

    changes in build, packer, and shell

--- app.sh
+++ app.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 echo "Installing Open JDK"
 sudo apt-get install openjdk-19-jdk -y
 echo "Java installed successfully"
 echo "$(java --version) is the version of java"
 
-sudo mkdir /home/ubuntu/webapp
-sudo mv /tmp/unichef.jar /home/ubuntu/webapp
+echo "Creating new group and users"
+sudo groupadd unichef
+sudo useradd -s /bin/false -g unichef -d /home/unichef -m unichef
+
+sudo mv /tmp/unichef.jar /home/unichef
 sudo mv /tmp/unichef.service /etc/systemd/system/unichef.service
+
+cd /home/unichef
+sudo chown unichef:unichef ./unichef.jar
+sudo chmod +x ./unichef.jar
+
+sudo systemctl daemon-reload
 sudo systemctl enable unichef.service
 sudo systemctl start unichef.service
